7984d92a628e770420c6e6723ab6e7b345dc549b
Majoxs <majoxs@gmail.com>
2022-05-23 10:33:11 +0500

    Added info, characteristics and slider blocks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-new Swiper('.bike-slider', {
+const bikeSlider = document.querySelector('.bike-slider');
+const mainSlider = document.querySelector('.slider-main');
+
+new Swiper(bikeSlider, {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -22,4 +25,25 @@ new Swiper('.bike-slider', {
   fadeEffect: {
     crossFade: true
   },
+});
+
+new Swiper(mainSlider, {
+  pagination: {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
+  loop: true,
+  simulateTouch: true,
+  touchRatio: 2,
+  touchAngle: 45,
+  mousewheel: {
+    sensitivity: 1,
+    eventsTarget: '.bike-slider'
+  },
+  slidesPerView: 1,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  speed: 300,
 });
